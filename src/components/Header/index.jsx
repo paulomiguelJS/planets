6a12c34171b7ee3e6c52c4d535a30fbde58d236c
Paulo <paulomiguelblues@gmail.com>
2022-05-02 00:00:45 -0400

    Planets added, background implemented

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,18 +4,18 @@ const Header = () => {
   return (
       <header className='flex justify-between items-center p-4'>
            <div>
-        <h1>The Planets</h1>
+        <h1 className='text-[#ffffff]'>THE PLANETS</h1>
         </div>
         <nav >
-       <ul className='flex flex-row '>
-           <li>MERCURY</li>
-           <li>VENUS</li>
-           <li>EARTH</li>
-           <li>MARS</li>
-           <li>JUPITER</li>
-           <li>SATURN</li>
-           <li>URANUS</li>
-           <li>NEPTUNE</li>
+       <ul className='flex flex-row text-[#c1c1c8] '>
+           <li className='mx-3'><a href="">MERCURY</a></li>
+           <li className='mx-3'><a href="">VENUS</a></li>
+           <li className='mx-3'><a href="">EARTH</a></li>
+           <li className='mx-3'><a href="">MARS</a></li>
+           <li className='mx-3'><a href="">JUPITER</a></li>
+           <li className='mx-3'><a href="">SATURN</a></li>
+           <li className='mx-3'><a href="">URANUS</a></li>
+           <li className='mx-3'><a href="">NEPTUNE</a></li>
        </ul>
        </nav>
       </header>
