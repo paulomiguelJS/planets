b4ac60e0cd2cd43cc6dae437c07c932ee7493666
Paulo <paulomiguelblues@gmail.com>
2022-05-03 14:59:28 -0400

    Font-family added, header styled

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,18 +4,18 @@ const Header = () => {
   return (
       <header className='flex justify-between items-center p-4'>
            <div>
-        <h1 className='text-[#ffffff]'>THE PLANETS</h1>
+        <h1 className='text-[#ffffff] font-title text-3xl cursor-pointer'>THE PLANETS</h1>
         </div>
         <nav >
-       <ul className='flex flex-row text-[#c1c1c8] '>
-           <li className='mx-3'><a href="">MERCURY</a></li>
-           <li className='mx-3'><a href="">VENUS</a></li>
-           <li className='mx-3'><a href="">EARTH</a></li>
-           <li className='mx-3'><a href="">MARS</a></li>
-           <li className='mx-3'><a href="">JUPITER</a></li>
-           <li className='mx-3'><a href="">SATURN</a></li>
-           <li className='mx-3'><a href="">URANUS</a></li>
-           <li className='mx-3'><a href="">NEPTUNE</a></li>
+       <ul className='flex flex-row text-[#c1c1c8] font-title font-bold tracking-widest '>
+           <li className='mx-3 hover:text-white'><a href="">MERCURY</a></li>
+           <li className='mx-3 hover:text-white'><a href="">VENUS</a></li>
+           <li className='mx-3 hover:text-white'><a href="">EARTH</a></li>
+           <li className='mx-3 hover:text-white'><a href="">MARS</a></li>
+           <li className='mx-3 hover:text-white'><a href="">JUPITER</a></li>
+           <li className='mx-3 hover:text-white'><a href="">SATURN</a></li>
+           <li className='mx-3 hover:text-white'><a href="">URANUS</a></li>
+           <li className='mx-3 hover:text-white'><a href="">NEPTUNE</a></li>
        </ul>
        </nav>
       </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    
     extend: {
       colors: {
         "grey-border": "#979797",
       },
+      fontFamily: {
+        title: ['Antonio'],
+        body: ['League Spartan']
+      }
     },
   },
   plugins: [],
