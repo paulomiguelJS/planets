4574be27da5c4f462aa5e6dc4d038b889d4c671b
Filipe Gomes <filipe.gt1998@outlook.com>
2022-05-08 19:22:56 -0300

    Este commit adiciona logica dos planetas

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -2,14 +2,21 @@ import React, { useEffect, useState } from "react";
 import Cards from "../components/Cards";
 import Header from "../components/Header";
 import Planet from "../components/Planet";
+import { planets as planetsList } from "../services/data";
 
 const Home = () => {
+  const [planetId, setPlanetId] = useState(1);
+  const [planetData, setPlanetData] = useState([]);  
 
+
+  useEffect(() => {
+    planetsList.map((infoPlanets) => infoPlanets.id === planetId && setPlanetData(infoPlanets));
+  }, [planetId])
 
   return (
     <>
-      <Header />
-      <Planet />
+      <Header setPlanetId={setPlanetId}/>
+      <Planet planetData={planetData}/>
       <Cards />
     </>
   );
--- src/components/Planet/index.jsx
+++ src/components/Planet/index.jsx
@@ -1,29 +1,17 @@
 import React from 'react'
 import {planets} from '../../services/data'
 import Button from '../Button'
-import pic from "../../assets/planet-neptune.svg";
 
-
-const Planet = () => {
-
-
-
+const Planet = ({planetData}) => {
 
 
   return (
     <main className='p-2 mt-12 container mx-auto text-white flex items-center justify-around'>
-            <img className="basis-1/3 transform transition duration-1000 hover:scale-110  "  src={pic} />
+        <img className="basis-1/3 transform transition duration-1000 hover:scale-110" src={planetData?.planetover} alt={'Imagem'} style={{height: '500px'}}/>
         <aside className='basis-1/4'>
 
-
-          {planets.map((planet) => (
-            <div>{planet.name}</div>
-          ))}
-
-          
-            <p className='font-body text-base mb-3'>Much of the Venusian surface appears to have been shaped by volcanic activity. 
-                Venus has several times as many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across.
-                 The only volcanic complex of this size on Earth is the Big Island of Hawaii.</p>
+            <h1 className='font-title text-7xl mb-10'>{planetData?.name}</h1>        
+            <p className='font-body text-base mb-3'>{planetData?.overview}</p>
             <div>
                 <span>Source:</span>
                 <a href="#">Wikipedia </a>
