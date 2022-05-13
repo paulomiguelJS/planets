5d8e82435c4f351c418e7d2ece926f02b54db690
Paulo <paulomiguelblues@gmail.com>
2022-05-13 16:15:10 -0400

    mobile list resized

--- src/components/Header/NavMobile/index.jsx
+++ src/components/Header/NavMobile/index.jsx
@@ -5,13 +5,13 @@ const MenuItems = ({ setPlanetId, active }) => {
     <ul
       className={
         active
-          ? "flex flex-col absolute left-0 mt-14 gap-10 bg-transparent bg-[#070724] z-10 w-full h-screen  p-8"
+          ? "flex flex-col absolute left-0 mt-4 gap-10 bg-transparent bg-[#070724] z-10 w-full h-screen  p-8"
           : "hidden"
       }
     >
       {PlanetInfo.map((planet, index) => (
         <li
-          className="relative flex items-center before:block before:absolute before:w-5 before:h-5 before:rounded-full before:-left-5 before:bg-teal-500 after:block after:absolute after:w-14 after:h-14 after:bg-[url('./assets/arrow.svg')] after:bg-no-repeat after:bg-center after:right-0 pl-6 py-3 font-bold text-lg text-white bg-transparent cursor-pointer " 
+          className="relative flex items-center before:block before:absolute before:w-5 before:h-5 before:rounded-full before:-left-5 before:bg-teal-500 after:block after:absolute after:w-14 after:h-14 after:bg-[url('./assets/arrow.svg')] after:bg-no-repeat after:bg-center after:right-0 pl-6 py-3 font-bold text-base text-white bg-transparent cursor-pointer " 
           key={index}
           onClick={() => setPlanetId(planet.id)}
         >
